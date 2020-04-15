5fea1a7f34ae89d32d4f30d4a43cd48688c6997d
Alexey Solovey <solovei_94@mail.ru>
2020-04-15 10:39:20 +0400

    create project "Message of the day"

---
+++ src/main/java/com/solovey/messageoftheday/MessageOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package com.solovey.messageoftheday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageOfTheDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageOfTheDayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solovey/messageoftheday/MessageOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solovey.messageoftheday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageOfTheDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
