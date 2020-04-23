0b22598db670952b7072b27b5937864269188c26
Alexey Solovey <solovei_94@mail.ru>
2020-04-23 23:22:00 +0400

    refactor

--- src/main/java/com/solovey/messageoftheday/MessageOfTheDayApplication.java
+++ src/main/java/com/solovey/messageoftheday/MessageOfTheDayApplication.java
@@ -1,13 +1,45 @@
 package com.solovey.messageoftheday;
 
+import com.solovey.messageoftheday.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
-public class MessageOfTheDayApplication {
+public class MessageOfTheDayApplication  implements CommandLineRunner {
+
+    @Autowired
+    CustomerService customerService;
 
     public static void main(String[] args) {
         SpringApplication.run(MessageOfTheDayApplication.class, args);
     }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Override
+    public void run(String... params) throws Exception {
+/*        Customer admin = new Customer();
+        admin.setUsername("admin");
+        admin.setPassword("admin");
+        admin.setRoles(new ArrayList<Role>(Arrays.asList(Role.ROLE_ADMIN)));
+
+        customerService.signUp(admin);
+
+        Customer client = new Customer();
+        client.setUsername("client");
+        client.setPassword("client");
+        client.setRoles(new ArrayList<Role>(Arrays.asList(Role.ROLE_CLIENT)));
+
+        customerService.signUp(client);*/
+    }
+
+
 
 }
